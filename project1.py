861731cc6f2b69e3a946fa8e44f35576fd55fc9d
Harkirats043 <Harkirats043@gmail.com>
2023-10-05 22:48:05 -0400

    create players

--- project1.py
+++ project1.py
@@ -8,7 +8,15 @@ def roll():
     
     return roll
 
-value = roll()
-print(value)
-
-
+while True:
+   players = input("Enter the number of players(1-4): ")
+   if players.isdigit():
+       players = int(players)
+       if 1 <= players <= 4:
+           break
+       else:
+           print("Must be between 2-4 players.")
+   else:
+        print("Invalid, try again.")
+        
+print(players)
